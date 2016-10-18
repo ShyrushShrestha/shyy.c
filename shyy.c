64bc91590c3a342ce94a7881f9dce4afdfeecf19
ShyrushShrestha <Shyrush.sresta1@gmail.com>
2016-10-18 10:52:29 +0545

    Add files via upload

---
+++ shyy.c
@@ -0,0 +1,21 @@
+/*
+Student name:Shyrush Shrestha
+Subject: Programming Fundam.
+LabNo:02
+Program:
+Date: 2016-10-18
+*/
+#include<stdio.h>
+//#include<conio.h>
+int main(){
+float firstNumber,secondNumber,add;
+
+	printf("Enter the First Number.:");
+	scanf("%f",&firstNumber);
+	printf("Enter the Second Number.:");
+	scanf("%f",&secondNumber);
+	add=firstNumber+secondNumber;	
+	printf("Use of '+'Operator :\n");
+	printf("%f + %f = %f",firstNumber,secondNumber,add);
+return 0;
+	}
